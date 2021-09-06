919bdfa86d1562efcb0ac098ec40da0734226c15
Dimmero <54788826+Dimmero@users.noreply.github.com>
2021-09-06 19:59:37 +0200

    6. FloatDoubleChallenge30 done

---
+++ src/main/java/Section_3/challenges/FloatDoubleChallenge30.java
@@ -0,0 +1,10 @@
+package Section_3.challenges;
+
+public class FloatDoubleChallenge30 {
+    public static void main(String[] args) {
+        double onePound = 0.45359237;
+        double amountOfPounds = 400d;
+        double amountOfKilograms = amountOfPounds * onePound;
+        System.out.println(amountOfKilograms);
+    }
+}
