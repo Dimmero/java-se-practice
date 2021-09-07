3d87bf88b7596b38cd599b0ed8ea7fc5653ff9da
Dima Akulich <akudzm@gmail.com>
2021-09-07 15:31:38 +0200

    8. 49. More on methods and a challenge (with StringBuffer, arrays of players and scores, highScore1 is for Name1)

--- src/main/java/Section4/Methods.java
+++ src/main/java/Section4/Methods.java
@@ -1,38 +1,63 @@
 package Section4;
 
 public class Methods {
+    public static void main(String[] args) {
+        String Name1 = "Dima";
+        String Name2 = "Karol";
+        String Name3 = "Kornel";
+        String Name4 = "Alex";
+        String[] arrayPlayers = {Name1, Name2, Name3, Name4};
 
-public static void main(String[] args) {
-    boolean gameOver = true;
-    int score = 5000;
-    int levelCompleted = 5;
-    int bonus = 100;
+        int highScore1 = 1500;
+        int highScore2 = 900;
+        int highScore3 = 400;
+        int highScore4 = 50;
+        int[] arrayScores = {highScore1, highScore2, highScore3, highScore4};
 
-    calculateScore();
+//        int highScore = calculateScore(true, 800, 5, 100);
+//        System.out.println("Your final score was " + highScore);
+//
+//        int newHighScore = calculateScore(true, 10000, 8, 200);
+//        System.out.println("Your final score was " + newHighScore);
 
-    boolean newGameOver = true;
-    int newScore = 10000;
-    int newLevelCompleted = 8;
-    int newBonus = 200;
-
-    if (newGameOver){
-        int newFinalScore = newScore + (newLevelCompleted * newBonus);
-        System.out.println("Your final score is " + newFinalScore);
-    }
-
-}
-    public static void calculateScore(){
-        boolean gameOver = true;
-        int score = 5000;
-        int levelCompleted = 5;
-        int bonus = 100;
-
-        if(gameOver){
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
+        int counter = 0;
+        for (int score : arrayScores
+        ) {
+            int scorePosition = calculateHighScorePosition(score);
+            StringBuffer displayPosition = displayHighScorePosition(arrayPlayers[counter], scorePosition);
+            counter++;
+            System.out.println(displayPosition);
         }
     }
 
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            return finalScore;
+        }
+        return -1;
     }
+
+    public static StringBuffer displayHighScorePosition(String player, int position) {
+        StringBuffer displayPosition = new StringBuffer();
+        displayPosition.append(player)
+                .append(" managed to get into position ")
+                .append(position)
+                .append(" on the high score table");
+        return displayPosition;
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score > 1000)
+            return 1;
+        else if (score > 500 && score < 1000)
+            return 2;
+        else if (score > 100 && score < 500)
+            return 3;
+        else
+            return 4;
+    }
+}
 
 
