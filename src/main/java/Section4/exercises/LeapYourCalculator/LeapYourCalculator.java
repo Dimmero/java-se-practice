989492493cc0a5f3bc57f3269769dde3ad5aeb5e
Dima Akulich <akudzm@gmail.com>
2021-09-08 14:11:50 +0200

    14. LeapYear (fixed)

--- src/main/java/Section4/exercises/LeapYourCalculator/LeapYourCalculator.java
+++ src/main/java/Section4/exercises/LeapYourCalculator/LeapYourCalculator.java
@@ -7,6 +7,7 @@ public class LeapYourCalculator {
         } else if (year % 4 == 0) {
             if (year % 100 == 0) {
                 if (year % 400 == 0) {
+                    return true;
                 } else return false;
             } else return true;
         }
