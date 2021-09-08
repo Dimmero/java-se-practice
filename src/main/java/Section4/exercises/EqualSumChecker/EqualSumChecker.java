7fe66d1070fe2eda6dda369e04d28f05e7c4aed2
Dima Akulich <akudzm@gmail.com>
2021-09-08 15:21:25 +0200

    15. EqualSumChecker

---
+++ src/main/java/Section4/exercises/EqualSumChecker/EqualSumChecker.java
@@ -0,0 +1,8 @@
+package Section4.exercises.EqualSumChecker;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int x, int y, int z){
+//        System.out.println(x + y == z);
+        return x + y == z;
+    }
+}
