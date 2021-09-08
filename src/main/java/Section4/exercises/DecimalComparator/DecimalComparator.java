d32a27044afa2a03c51ba329b0975651200c14c0
Dima Akulich <akudzm@gmail.com>
2021-09-08 14:55:26 +0200

    15. DecimalComparator

---
+++ src/main/java/Section4/exercises/DecimalComparator/DecimalComparator.java
@@ -0,0 +1,7 @@
+package Section4.exercises.DecimalComparator;
+
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double number1, double number2){
+        return (int) (number1 * 1000) == (int) (number2 * 1000);
+    }
+}
