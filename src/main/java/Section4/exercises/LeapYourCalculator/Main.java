d2579bdd686940bffd64d7826da300c3f3a2ec61
Dima Akulich <akudzm@gmail.com>
2021-09-08 14:08:52 +0200

    14. LeapYear

---
+++ src/main/java/Section4/exercises/LeapYourCalculator/Main.java
@@ -0,0 +1,37 @@
+package Section4.exercises.LeapYourCalculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = 0;
+//        int result = a > 10 ? b = 3  : b++;
+//        System.out.println(b);
+//        switch(a){
+//            case 3, 6, 7 -> System.out.println("That's 3");
+//            case 4 -> System.out.println("That's 4");
+//            case 5 -> System.out.println("That's 5");
+//        }
+        System.out.println(LeapYourCalculator.isLeapYear(1924));
+        System.out.println(LeapYourCalculator.isLeapYear(2400));
+        System.out.println(LeapYourCalculator.isLeapYear(-2400));
+        System.out.println(LeapYourCalculator.isLeapYear(1800));
+        System.out.println(LeapYourCalculator.isLeapYear(1900));
+        System.out.println(LeapYourCalculator.isLeapYear(2100));
+        System.out.println(LeapYourCalculator.isLeapYear(2200));
+        System.out.println(LeapYourCalculator.isLeapYear(2300));
+        System.out.println(LeapYourCalculator.isLeapYear(2500));
+        System.out.println(LeapYourCalculator.isLeapYear(2600));
+        System.out.println(LeapYourCalculator.isLeapYear(1600));
+        System.out.println(LeapYourCalculator.isLeapYear(2000));
+        System.out.println(LeapYourCalculator.isLeapYear(2400));
+        System.out.println(LeapYourCalculator.isLeapYear(1924));
+
+
+
+
+        }
+    }
+
