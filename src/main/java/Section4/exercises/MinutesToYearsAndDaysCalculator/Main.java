95a4901af3255a849e48aecdb7d7b37c00029d53
Dima Akulich <akudzm@gmail.com>
2021-09-09 17:03:52 +0200

    19. MinutesToYearsAndDaysCalculator

---
+++ src/main/java/Section4/exercises/MinutesToYearsAndDaysCalculator/Main.java
@@ -0,0 +1,9 @@
+package Section4.exercises.MinutesToYearsAndDaysCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        MinutesToYearsAndDaysCalculator.printYearsAndDays(-525600);
+        MinutesToYearsAndDaysCalculator.printYearsAndDays(0);
+        MinutesToYearsAndDaysCalculator.printYearsAndDays(561600);
+    }
+}
