c788329dd3060a67fed25dbe8353074d9cd098f7
Dima Akulich <akudzm@gmail.com>
2021-09-09 18:11:18 +0200

    21. PlayingCat

---
+++ src/main/java/Section4/exercises/PlayingCat/PlayingCat.java
@@ -0,0 +1,12 @@
+package Section4.exercises.PlayingCat;
+
+public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if (temperature >= 25 && temperature <= 35){
+            return true;
+        }else if (summer && temperature - 10 >= 25 && temperature - 10 <= 35){
+            return true;
+        }else
+            return false;
+    }
+}
