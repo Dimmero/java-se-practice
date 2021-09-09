36178e7bb353994fccb39c669f5eb8ba5431d8f8
Dima Akulich <akudzm@gmail.com>
2021-09-09 17:52:04 +0200

    20. EqualityPrinter

---
+++ src/main/java/Section4/exercises/EqualityPrinter/Main.java
@@ -0,0 +1,11 @@
+package Section4.exercises.EqualityPrinter;
+
+public class Main {
+    public static void main(String[] args) {
+        EqualityPrinter.printEqual(-1, 1, -1);
+        EqualityPrinter.printEqual(1, 1, 1);
+        EqualityPrinter.printEqual(1, 1, 2);
+        EqualityPrinter.printEqual(1, 2, 3);
+
+    }
+}
