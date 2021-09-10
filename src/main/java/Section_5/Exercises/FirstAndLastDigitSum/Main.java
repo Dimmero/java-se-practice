59ff29865d2d7cc0911e13d28b511ed7c3ace278
Dima Akulich <akudzm@gmail.com>
2021-09-10 22:50:21 +0200

    25. FirstAndLastDigitSum

---
+++ src/main/java/Section_5/Exercises/FirstAndLastDigitSum/Main.java
@@ -0,0 +1,12 @@
+package Section_5.Exercises.FirstAndLastDigitSum;
+
+public class Main {
+    public static void main(String[] args) {
+        FirstAndLastDigitSum.sumFirstAndLastDigit(0);
+        FirstAndLastDigitSum.sumFirstAndLastDigit(5);
+        FirstAndLastDigitSum.sumFirstAndLastDigit(12345);
+        System.out.println(FirstAndLastDigitSum.sumFirstAndLastDigit(0));
+        System.out.println(FirstAndLastDigitSum.sumFirstAndLastDigit(5));
+        System.out.println(FirstAndLastDigitSum.sumFirstAndLastDigit(12345));
+    }
+}
--- src/main/java/Section_5/Exercises/NumberPalindrome/Main.java
+++ src/main/java/Section_5/Exercises/NumberPalindrome/Main.java
@@ -3,5 +3,6 @@ package Section_5.Exercises.NumberPalindrome;
 public class Main {
     public static void main(String[] args) {
         System.out.println(NumberPalindrome.isPalindrome(-12321));
+        System.out.println(NumberPalindrome.isPalindrome(123454321));
     }
 }
