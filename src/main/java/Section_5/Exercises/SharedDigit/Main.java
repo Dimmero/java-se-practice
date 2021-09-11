74c8b342b30bccaaddabe9ce5f6e32f5c1b8770f
Dima Akulich <akudzm@gmail.com>
2021-09-11 14:05:12 +0200

    27. SharedDigit

---
+++ src/main/java/Section_5/Exercises/SharedDigit/Main.java
@@ -0,0 +1,11 @@
+package Section_5.Exercises.SharedDigit;
+
+public class Main {
+    public static void main(String[] args) {
+//        SharedDigit.hasSharedDigit(2, 100);
+//        System.out.println(SharedDigit.hasSharedDigit(2, 100));
+        SharedDigit.hasSharedDigit(12, 13);
+        System.out.println(SharedDigit.hasSharedDigit(12, 13));
+
+    }
+}
