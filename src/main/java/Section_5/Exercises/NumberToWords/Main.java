f1437ff019c7d3313362d17ec5110a190dc6f004
Dima Akulich <akudzm@gmail.com>
2021-09-12 23:04:17 +0200

    31. NumberToWords with cleaner code

--- src/main/java/Section_5/Exercises/NumberToWords/Main.java
+++ src/main/java/Section_5/Exercises/NumberToWords/Main.java
@@ -3,7 +3,6 @@ package Section_5.Exercises.NumberToWords;
 public class Main {
     public static void main(String[] args) {
         NumberToWords.numberToWords(100);
-        System.out.println();
-//        System.out.println(NumberToWords.getDigitCount(101));
+
     }
 }
--- src/main/java/Section_5/Exercises/NumberToWords/NumberToWords.java
+++ src/main/java/Section_5/Exercises/NumberToWords/NumberToWords.java
@@ -1,71 +1,43 @@
 package Section_5.Exercises.NumberToWords;
 
 public class NumberToWords {
-    public static void numberToWords(int number){
+    public static void numberToWords(int number) {
         if (number < 0)
             System.out.println("Invalid Value");
+
         boolean numberIsZero = true;
-        int numberToPrint = 0;
+        int numberToPrint;
         int reversedNumber = reverse(number);
-        while (numberIsZero){
-            numberToPrint = reversedNumber % 10;
-            reversedNumber /= 10;
+
+        while (numberIsZero) {
             if (reversedNumber == 0)
                 numberIsZero = false;
-            switch (numberToPrint){
-                case 0: {
-                    System.out.print("Zero ");
-                    break;
-                }
-                case 1: {
-                    System.out.print("One ");
-                    break;
-                }
-                case 2: {
-                    System.out.print("Two ");
-                    break;
-                }
-                case 3: {
-                    System.out.print("Three ");
-                    break;
-                }
-                case 4: {
-                    System.out.print("Four ");
-                    break;
-                }
-                case 5: {
-                    System.out.print("Five ");
-                    break;
-                }
-                case 6: {
-                    System.out.print("Six ");
-                    break;
-                }
-                case 7: {
-                    System.out.print("Seven ");
-                    break;
-                }
-                case 8: {
-                    System.out.print("Eight ");
-                    break;
-                }
-                case 9: {
-                    System.out.print("Nine ");
-                    break;
-                }
+            numberToPrint = reversedNumber % 10;
+            reversedNumber /= 10;
+            switch (numberToPrint) {
+                case 0 -> System.out.print("Zero ");
+                case 1 -> System.out.print("One ");
+                case 2 -> System.out.print("Two ");
+                case 3 -> System.out.print("Three ");
+                case 4 -> System.out.print("Four ");
+                case 5 -> System.out.print("Five ");
+                case 6 -> System.out.print("Six ");
+                case 7 -> System.out.print("Seven ");
+                case 8 -> System.out.print("Eight ");
+                case 9 -> System.out.print("Nine ");
             }
         }
-        for (int i = 0; i < (getDigitCount(number) - getDigitCount((int) reverse(number))); i++) {
+        for (int i = 0; i < (getDigitCount(number) - getDigitCount(reverse(number))); i++) {
             System.out.print("Zero ");
         }
     }
 
-    public static int reverse(int number){
+    public static int reverse(int number) {
         int reversedNumber = 0;
-        int remainingNumber = 0;
+        int remainingNumber;
         boolean numberIsZero = true;
 
-        while (numberIsZero){
+        while (numberIsZero) {
             remainingNumber = number % 10;
             number /= 10;
             reversedNumber = reversedNumber * 10 + remainingNumber;
@@ -84,7 +56,7 @@ public class NumberToWords {
         boolean numberIsZero = true;
         int counter = 0;
 
-        while (numberIsZero){
+        while (numberIsZero) {
             number /= 10;
             counter++;
             if (number == 0)
