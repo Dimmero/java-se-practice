3dc7b90aeae345c4d4e0137c9dce9aef7a2489a8
Dima Akulich <akudzm@gmail.com>
2021-09-12 20:06:43 +0200

    28. GreatestCommonDivisor with better performance

---
+++ src/main/java/Section_5/Exercises/GratestCommonDivisor/Main.java
@@ -0,0 +1,7 @@
+package Section_5.Exercises.GratestCommonDivisor;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(25 * 520, 25 * 919203));
+    }
+}
