fbe4caca809a8617640bb50da9522b794b25f83e
Dima Akulich <akudzm@gmail.com>
2021-09-13 17:37:23 +0200

    Ex.25 LargestPrime

---
+++ src/main/java/Section_5/Exercises/LargestPrime/Main.java
@@ -0,0 +1,9 @@
+package Section_5.Exercises.LargestPrime;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(LargestPrime.isPrimeNumber(19));
+        System.out.println(LargestPrime.getLargestPrime(45));
+
+    }
+}
