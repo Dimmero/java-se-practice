0884568a6fc9be6db4db6cf74734915adc241ef8
Dima Akulich <akudzm@gmail.com>
2021-09-15 02:10:52 +0200

    Ex.27 InputCalculator

---
+++ src/main/java/Section_5/InputCalculator/InputCalculator.java
@@ -0,0 +1,20 @@
+package Section_5.InputCalculator;
+
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage() {
+        int sum = 0;
+        int counter = 0;
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        while (scanner.hasNextInt()) {
+            number = scanner.nextInt();
+            sum += number;
+            counter++;
+        }
+        long avg = Math.round((double) sum / counter);
+        System.out.printf("SUM = %d AVG = %d", sum, avg);
+        scanner.close();
+    }
+}
---
+++ src/main/java/Section_5/InputCalculator/Main.java
@@ -0,0 +1,7 @@
+package Section_5.InputCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        InputCalculator.inputThenPrintSumAndAverage();
+    }
+}
