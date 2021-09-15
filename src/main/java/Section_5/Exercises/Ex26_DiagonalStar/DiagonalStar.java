fa0308f0d1547a1db1de66753ece7c745f57d5b1
Dima Akulich <akudzm@gmail.com>
2021-09-15 19:48:45 +0200

    4 and 5 Sections Done

--- src/main/java/Section_5/Exercises/Ex26_DiagonalStar/DiagonalStar.java
+++ src/main/java/Section_5/Exercises/Ex26_DiagonalStar/DiagonalStar.java
@@ -2,15 +2,26 @@ package Section_5.Exercises.Ex26_DiagonalStar;
 
 public class DiagonalStar {
     public static void printSquareStar(int number){
-        if (number < 5){
-            System.out.println("Invalid Value");
-        }
-        for (int i = 0; i < number; i++) {
-            for (int j = number; j > i ; j--) {
-
-                System.out.print("*");
+        if(number < 5) System.out.println("Invalid Value");
+        else{
+            for (int row = 0; row <number; row++){
+                for (int column=0; column<number; column++){
+                    if (row == 0 || row == number - 1){
+                        System.out.print("*");
+                    } else if (column == 0){
+                        System.out.print("*");
+                    } else if (column == number - 1){
+                        System.out.print("*");
+                    } else if (column == row){
+                        System.out.print("*");
+                    }else if (column == number - 1 - row){
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println("");
             }
-            System.out.println("");
         }
     }
 }
--- src/main/java/Section_5/Exercises/Ex26_DiagonalStar/Main.java
+++ src/main/java/Section_5/Exercises/Ex26_DiagonalStar/Main.java
@@ -2,6 +2,6 @@ package Section_5.Exercises.Ex26_DiagonalStar;
 
 public class Main {
     public static void main(String[] args) {
-        DiagonalStar.printSquareStar(6);
+        DiagonalStar.printSquareStar(5);
     }
 }
