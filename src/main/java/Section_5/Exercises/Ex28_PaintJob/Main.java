065ec7043f20f67e8ef3fdd69fe5051a4df790ce
Dima Akulich <akudzm@gmail.com>
2021-09-15 18:03:07 +0200

    4 and 5 Sections Done (Exception: Ex-s 26) ex 28 with extra method

--- src/main/java/Section_5/Exercises/Ex28_PaintJob/Main.java
+++ src/main/java/Section_5/Exercises/Ex28_PaintJob/Main.java
@@ -2,6 +2,9 @@ package Section_5.Exercises.Ex28_PaintJob;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(PaintJob.getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(PaintJob.getBucketCount(0.75, 0.75, 0.5, 0));
+        System.out.println(PaintJob.getBucketCount(3.26,2.15,0.75));
+        System.out.println(PaintJob.getBucketCount(3.26,0.75));
     }
 }
--- src/main/java/Section_5/Exercises/Ex28_PaintJob/PaintJob.java
+++ src/main/java/Section_5/Exercises/Ex28_PaintJob/PaintJob.java
@@ -1,4 +1,39 @@
 package Section_5.Exercises.Ex28_PaintJob;
 
 public class PaintJob {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+        double area = width * height;
+        double needBucket = area / areaPerBucket;
+        if (notWholeNumber(needBucket))
+            return (int)(needBucket - extraBuckets) + 1;
+        else return (int)(area / areaPerBucket - extraBuckets);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        double area = width * height;
+        double needBucket = area / areaPerBucket;
+        if (notWholeNumber(needBucket)){
+            return (int) (area / areaPerBucket + 1);
+        }else return (int) (area / areaPerBucket);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        double needBucket = area / areaPerBucket;
+        if (notWholeNumber(needBucket)){
+            return (int) (area / areaPerBucket) + 1;
+        } else return (int) (area / areaPerBucket) ;
+    }
+
+    public static boolean notWholeNumber(double needBucket){
+        return needBucket % 1 != 1;
+    }
 }
